59bc3b80f980a145693611bdeeea858949c85284
Basanth <basanthsk@gmail.com>
2023-06-16 19:16:37 +0530

    testing

--- src/chickenClassifier/pipeline/stage_04_evaluation.py
+++ src/chickenClassifier/pipeline/stage_04_evaluation.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = EvaluationPipeline()
         obj.main()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+        logger.info(f">>>>>> stage {STAGE_NAME}completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
         raise e
